ad86b14c3ee368ebcb960b5de5b29af983b83fa2
Jay Bergonia <jaybergonia@Jays-Mac-mini.local>
2021-09-04 20:15:18 +0800

    question class added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,13 +28,13 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
-
-  List<bool> answers = [false, true, true];
 
   int questionNumber = 0;
 
@@ -50,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,7 +79,8 @@ class _QuizPageState extends State<QuizPage> {
                 //TODO: The user picked true.
 
                 // check if user answer is Correct or NOT
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer =
+                    questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == true) {
                   print('user got it right');
                 } else {
@@ -111,7 +113,8 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked false.
 
                 // check if user answer is Correct or NOT
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer =
+                    questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == true) {
                   print('user got it right');
                 } else {
---
+++ lib/question.dart
@@ -0,0 +1,10 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  Question({String q, bool a}) {
+    // Constructor
+    questionText = q;
+    questionAnswer = a;
+  }
+}
