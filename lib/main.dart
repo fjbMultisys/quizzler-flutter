739c5492068d282b3f893c751a0335b2bb084bc5
Jay Bergonia <jaybergonia@Jays-Mac-mini.local>
2021-09-04 18:46:35 +0800

    questions set

--- lib/main.dart
+++ lib/main.dart
@@ -25,19 +25,30 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
+        //MARK: This is where the QUESTION text will go.
         Expanded(
           flex: 5,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -47,6 +58,7 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
+        //MARK: This is where the GREEN button go.
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
@@ -61,11 +73,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                //TODO: The user picked true.
+
+                questionNumber++;
               },
             ),
           ),
         ),
+        //MARK: This is where the RED button go.
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
@@ -85,6 +100,9 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
